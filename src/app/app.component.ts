c21622053823699d191062e5c68d8d7a430e0797
nicolasdichino <nicolasdichino@gmail.com>
2023-07-13 18:02:04 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  nombre: string;
+  costo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tarea_angular';
+  items: Item[] = [
+    { nombre: 'Producto 1', costo: '10$' },
+    { nombre: 'Producto 2', costo: '20$' },
+    { nombre: 'Producto 3', costo: '30$' }
+  ];
+
+  showList = true;
+
+  toggleList() {
+    this.showList = !this.showList;
+  }
 }
